10d2c5ab3a479e16097acd75eb4855bdc0db283d
NotInjured <akimbosss1@gmail.com>
2021-01-22 16:07:05 -0500

    Updated Lab-01

--- Labs/js/01-ZhiWeiWillySu-Lab.js
+++ Labs/js/01-ZhiWeiWillySu-Lab.js
@@ -4,83 +4,67 @@
 // Lab 1
 
 // Global Variables
-let scene, renderer, camera;
-let gui, trackballControls, clock, controls;
-let plane;
+let scene, renderer, camera, orbitControl, axesHelper;
+let materialCube, materialCylinder, materialBasic, materialPlane;
+let geoBox, geoSphere, geoCylinder, geoPlane, cube, sphere, cylinder, plane;
 
 // Function definitions
 function init() {
     scene = new THREE.Scene();
-    //stats = initStats();
-
     renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setClearColor(0x000000);
+    renderer.setClearColor(0x00DDFF);
     renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.shadowMapEnabled = true;
-    renderer.shadowMapSoft = true;
-
-    renderer.shadowCameraNear = 3;
-    renderer.shadowCameraFar = 500;
-    renderer.shadowCameraFov = 50;
-
-    renderer.shadowMapBias = 0.0039;
-    renderer.shadowMapDarkness = 0.5;
-    renderer.shadowMapWidth = 1024;
-    renderer.shadowMapHeight = 1024;
-
     document.body.appendChild(renderer.domElement);
 }
 
 function createCameraAndLights() {
-    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);    
-    camera.position.set( 0, 25, 50);
+    camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 100);    
+    camera.position.set(-10, 15, 10);
     camera.lookAt(scene.position);
-    clock = new THREE.Clock();//needed for the trackballControl
-    trackballControl = new THREE.TrackballControls(camera, renderer.domElement);
+    scene.add(camera);
 
-    // add hemisphere lighting
-    let hemisphereLight = new THREE.HemisphereLight(0xffffff, 0x080820, 0.5);
-    hemisphereLight.castShadow = true;
-    hemisphereLight.position.set(0, 500, 0);
-    scene.add(hemisphereLight);
-
-    // add directional lighting
-    let directionalLight = new THREE.DirectionalLight(0xffffff, 0.1);
-    directionalLight.castShadow = true;
-    directionalLight.position.set(40, 60, -10);
-    let dirHelper = new THREE.DirectionalLightHelper(directionalLight, 10);
-    directionalLight.add(dirHelper);
-
-    scene.add(directionalLight);
-
-    // add subtle ambient lighting
-    scene.add(new THREE.AmbientLight(0x595959, 0.5));
+    orbitControl = new THREE.OrbitControls(camera, renderer.domElement);
+    let pointLight = new THREE.PointLight(0xEEEEEE, 10, 50);
+    pointLight.position.set(-5, 5, -5);
+    scene.add(pointLight);
 }
 
 function createGeometry() {
-    let axesHelper = new THREE.AxesHelper(10);
+    axesHelper = new THREE.AxesHelper(5);
+    axesHelper.position.set(-15, -2, 0);
     scene.add(axesHelper);
+    // create material, geometry, and mesh
+    
+    materialCube = new THREE.MeshBasicMaterial({ color: 0xFF0000 });
+    geoBox = new THREE.BoxGeometry(1, 1, 1);
+    cube = new THREE.Mesh(geoBox, materialCube);
+    cube.position.set(5, -2, 0);
 
-    // create shape
+    materialCylinder = new THREE.MeshBasicMaterial({ color: 0x001FFF });
+    geoClyinder = new THREE.CylinderGeometry(1, 1, 5, 8, 2, false);
+    cylinder = new THREE.Mesh(geoClyinder, materialCylinder);
+    cylinder.position.set(-5, -2, 0);
 
-    // create plane material, geometry, and mesh
-    let mat = new THREE.MeshStandardMaterial({ color: 0x00FFD4 });
-    let geo = new THREE.PlaneGeometry(50, 50);
-    plane = new THREE.Mesh(geo, mat);
+    materialBasic = new THREE.MeshBasicMaterial({ color: 0xF0FF00 });
+    geoSphere = new THREE.SphereGeometry(1, 8, 8);
+    sphere = new THREE.Mesh(geoSphere, materialBasic);
+    sphere.position.set(0, -2, 0);
+
+    materialPlane = new THREE.MeshLambertMaterial({ color: 0xFFFFFF });
+    geoPlane = new THREE.PlaneBufferGeometry(60, 20);
+    plane = new THREE.Mesh(geoPlane, materialPlane);
     plane.rotation.x = -0.5 * Math.PI;
-    plane.position.x = 0;
-    plane.position.y = 0;
-    plane.position.z = 0;
-    plane.receiveShadow = true;
+    plane.position.set(0, -5, 0);
 
-    // add plane to scene
+    scene.add(cube);
+    scene.add(cylinder);
+    scene.add(sphere);
     scene.add(plane);
 }
 
 function render() {
-
     requestAnimationFrame(render);
-    trackballControl.update(clock.getDelta());
+    orbitControl.update();
     renderer.render(scene, camera);
 }
 
