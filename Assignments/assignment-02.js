aaecbf782f9f0d19a7684c5043972822d23a007c
NotInjured <akimbosss1@gmail.com>
2021-04-08 03:35:34 -0400

    Added Some Physics

--- Assignments/assignment-02.js
+++ Assignments/assignment-02.js
@@ -7,130 +7,135 @@ let orbitControls, controls, gui, levels, info;
 let jsonLoaded = false, raycasting = false, resetAdded = false;
 const objects = [], removed = [], min = [6, 10, 14, 18, 22];
 
-let Shaders = {}
-Shaders.TextureShader = {
-	name: 'TextureShader', //for debugging
-	uniforms: {
-        'textureA':{value: null}
-	},
+// let Shaders = {}
+// Shaders.TextureShader = {
+// 	name: 'TextureShader', //for debugging
+// 	uniforms: {
+//         'textureA':{value: null}
+// 	},
 	
-	vertexShader: 
-	`
-    uniform sampler2D textureA;
-	varying vec2  vUv;
+// 	vertexShader: 
+// 	`
+//     uniform sampler2D textureA;
+// 	varying vec2  vUv;
 
-	void main() {
-		vec3 pos = position;
-        vec4 color = texture2D(textureA, uv);
-        vUv = uv;
-		gl_Position = projectionMatrix * modelViewMatrix * vec4( pos, 1.0 );
-	}`,
+// 	void main() {
+// 		vec3 pos = position;
+//         vec4 color = texture2D(textureA, uv);
+//         vUv = uv;
+// 		gl_Position = projectionMatrix * modelViewMatrix * vec4( pos, 1.0 );
+// 	}`,
 
-	fragmentShader:
-	`
-    uniform sampler2D textureA;
-    varying vec2 vUv;
-    vec3 color = vec3(0.0, 0.0, 1.0);         //local variable red
+// 	fragmentShader:
+// 	`
+//     uniform sampler2D textureA;
+//     varying vec2 vUv;
+//     vec3 color = vec3(0.0, 0.0, 1.0);         //local variable red
 
-    void main() {
-        gl_FragColor = texture2D(textureA, vUv);
-    }`
-};
-Shaders.BoxShader1 = {
-	name: 'BoxShader1', //for debugging
-	uniforms: {
-        'textureA':{value: null}
-	},
+//     void main() {
+//         gl_FragColor = texture2D(textureA, vUv);
+//     }`
+// };
+// Shaders.BoxShader1 = {
+// 	name: 'BoxShader1', //for debugging
+// 	uniforms: {
+//         'textureA':{value: null}
+// 	},
 	
-	vertexShader: 
-	`
-    uniform sampler2D textureA;
-	varying vec2  vUv;
+// 	vertexShader: 
+// 	`
+//     uniform sampler2D textureA;
+// 	varying vec2  vUv;
 
-	void main() {
-		vec3 pos = position;
-        vec4 color = texture2D(textureA, uv);
-        vUv = uv;
-		gl_Position = projectionMatrix * modelViewMatrix * vec4( pos, 1.0 );
-	}`,
+// 	void main() {
+// 		vec3 pos = position;
+//         vec4 color = texture2D(textureA, uv);
+//         vUv = uv;
+// 		gl_Position = projectionMatrix * modelViewMatrix * vec4( pos, 1.0 );
+// 	}`,
 
-	fragmentShader:
-	`
-    uniform sampler2D textureA;
-    varying vec2 vUv;
-    vec3 color = vec3(0.0, 0.0, 1.0);         //local variable red
+// 	fragmentShader:
+// 	`
+//     uniform sampler2D textureA;
+//     varying vec2 vUv;
+//     vec3 color = vec3(0.0, 0.0, 1.0);         //local variable red
 
-    void main() {
-        gl_FragColor = texture2D(textureA, vUv);
-    }`
-};
-Shaders.BoxShader2 = {
-	name: 'BoxShader2', //for debugging
-	uniforms: {
-        'textureB':{value: null}
-	},
+//     void main() {
+//         gl_FragColor = texture2D(textureA, vUv);
+//     }`
+// };
+// Shaders.BoxShader2 = {
+// 	name: 'BoxShader2', //for debugging
+// 	uniforms: {
+//         'textureB':{value: null}
+// 	},
 	
-	vertexShader: 
-	`
-    uniform sampler2D textureB;
-	varying vec2  vUv;
+// 	vertexShader: 
+// 	`
+//     uniform sampler2D textureB;
+// 	varying vec2  vUv;
 
-	void main() {
-		vec3 pos = position;
-        vec4 color = texture2D(textureB, uv);
-        vUv = uv;
-		gl_Position = projectionMatrix * modelViewMatrix * vec4( pos, 1.0 );
-	}`,
+// 	void main() {
+// 		vec3 pos = position;
+//         vec4 color = texture2D(textureB, uv);
+//         vUv = uv;
+// 		gl_Position = projectionMatrix * modelViewMatrix * vec4( pos, 1.0 );
+// 	}`,
 
-	fragmentShader:
-	`
-    uniform sampler2D textureB;
-    varying vec2 vUv;
-    vec3 color = vec3(0.0, 0.0, 1.0);         //local variable red
+// 	fragmentShader:
+// 	`
+//     uniform sampler2D textureB;
+//     varying vec2 vUv;
+//     vec3 color = vec3(0.0, 0.0, 1.0);         //local variable red
 
-    void main() {
-        gl_FragColor = texture2D(textureB, vUv);
-    }`
-};
-Shaders.BoxShader3 = {
-	name: 'BoxShader3', //for debugging
-	uniforms: {
-        'textureC':{value: null}
-	},
+//     void main() {
+//         gl_FragColor = texture2D(textureB, vUv);
+//     }`
+// };
+// Shaders.BoxShader3 = {
+// 	name: 'BoxShader3', //for debugging
+// 	uniforms: {
+//         'textureC':{value: null}
+// 	},
 	
-	vertexShader: 
-	`
-    uniform sampler2D textureC;
-	varying vec2  vUv;
+// 	vertexShader: 
+// 	`
+//     uniform sampler2D textureC;
+// 	varying vec2  vUv;
 
-	void main() {
-		vec3 pos = position;
-        vec4 color = texture2D(textureC, uv);
-        vUv = uv;
-		gl_Position = projectionMatrix * modelViewMatrix * vec4( pos, 1.0 );
-	}`,
+// 	void main() {
+// 		vec3 pos = position;
+//         vec4 color = texture2D(textureC, uv);
+//         vUv = uv;
+// 		gl_Position = projectionMatrix * modelViewMatrix * vec4( pos, 1.0 );
+// 	}`,
 
-	fragmentShader:
-	`
-    uniform sampler2D textureC;
-    varying vec2 vUv;
-    vec3 color = vec3(0.0, 0.0, 1.0);         //local variable red
+// 	fragmentShader:
+// 	`
+//     uniform sampler2D textureC;
+//     varying vec2 vUv;
+//     vec3 color = vec3(0.0, 0.0, 1.0);         //local variable red
 
-    void main() {
-        gl_FragColor = texture2D(textureC, vUv);
-    }`
-};
-const __shaderT1 = Shaders.TextureShader;
-const __shaderT2 = Shaders.BoxShader1;
-const __shaderT3 = Shaders.BoxShader2;
-const __shaderT4 = Shaders.BoxShader3;
+//     void main() {
+//         gl_FragColor = texture2D(textureC, vUv);
+//     }`
+// };
+// const __shaderT1 = Shaders.TextureShader;
+// const __shaderT2 = Shaders.BoxShader1;
+// const __shaderT3 = Shaders.BoxShader2;
+// const __shaderT4 = Shaders.BoxShader3;
 
 const raycaster = new THREE.Raycaster();
 const mouse = new THREE.Vector2();
 
+Physijs.scripts.worker ='node_modules/physijs_worker.js';
+Physijs.scripts.ammo ='node_modules/ammo.js';
+const clock = new THREE.Clock();
+
 function init() {
+    scene = new Physijs.Scene();
+    scene.setGravity(new THREE.Vector3(0, -50, 0));
     renderer = new THREE.WebGLRenderer({ antialias: true });
-    scene = new THREE.Scene();
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setClearColor(0xFFFFFF);
@@ -162,21 +167,27 @@ function setupCameraAndLight() {
 function createGeometry() {
     scene.add(new THREE.AxesHelper(10));
 
-    textureMaterial = new THREE.ShaderMaterial(
-        {
-            uniforms: __shaderT1.uniforms,
-            vertexShader: __shaderT1.vertexShader,
-            fragmentShader: __shaderT1.fragmentShader
-        }
-    );
+    // textureMaterial = new THREE.ShaderMaterial(
+    //     {
+    //         uniforms: __shaderT1.uniforms,
+    //         vertexShader: __shaderT1.vertexShader,
+    //         fragmentShader: __shaderT1.fragmentShader
+    //     }
+    // );
 
-    __shaderT1.uniforms.textureA.value = new THREE.TextureLoader().load('assets/textures/Wood_Texture.png');
-    __shaderT2.uniforms.textureA.value = new THREE.TextureLoader().load('assets/textures/Iron_Block.png');
-    __shaderT3.uniforms.textureB.value = new THREE.TextureLoader().load('assets/textures/Gold_Block.png');
-    __shaderT4.uniforms.textureC.value = new THREE.TextureLoader().load('assets/textures/Diamond_Block.png');
+    // __shaderT1.uniforms.textureA.value = new THREE.TextureLoader().load('assets/textures/Wood_Texture.png');
+    // __shaderT2.uniforms.textureA.value = new THREE.TextureLoader().load('assets/textures/Iron_Block.png');
+    // __shaderT3.uniforms.textureB.value = new THREE.TextureLoader().load('assets/textures/Gold_Block.png');
+    // __shaderT4.uniforms.textureC.value = new THREE.TextureLoader().load('assets/textures/Diamond_Block.png');
 
-    table = new THREE.Mesh(new THREE.BoxBufferGeometry(50, 25, 0.5, 128, 128, 128), textureMaterial);
+    table = new Physijs.BoxMesh(
+        new THREE.BoxBufferGeometry(50, 25, 0.5),
+        Physijs.createMaterial(new THREE.MeshStandardMaterial(
+            {color: 0xeeeeee, map: new THREE.TextureLoader().load('assets/textures/Wood_Texture.png')},
+        0.2, 0.7)), 0)
+    // table = new THREE.Mesh(new THREE.BoxBufferGeometry(50, 25, 0.5, 128, 128, 128), textureMaterial);
     table.castShadow = true;
+    table.receiveShadow = true;
     table.rotation.x = -Math.PI * 0.5;
     table.name = "Table";
     scene.add(table);
@@ -279,10 +290,11 @@ function loadLevels(url){
 }
 
 function parseJson(){
-    let box = new THREE.Mesh(
-        new THREE.BoxBufferGeometry(4, 4, 4));
+    let box = new Physijs.BoxMesh(
+        new THREE.BoxBufferGeometry(4, 4, 4),
+        Physijs.createMaterial(new THREE.MeshLambertMaterial({transparent: true, opacity: 0.7, color: 0xffffff}),0.5, 0.98), 5)
     box.castShadow = true;
-    box.name = "iBlock";
+    box.receiveShadow = true;
 
     return box;
 }
@@ -292,30 +304,17 @@ function generateBlocks(){
         let block = parseJson();
         block.position.set(levels.boxPos[i][0], levels.boxPos[i][1], levels.boxPos[i][2]);
         if(levels.boxPos[i][3] == "I"){
-            block.material = new THREE.ShaderMaterial(
-                {
-                    uniforms: __shaderT2.uniforms,
-                    vertexShader: __shaderT2.vertexShader,
-                    fragmentShader: __shaderT2.fragmentShader
-                }
-            );
+            block.material = Physijs.createMaterial(new THREE.MeshStandardMaterial(
+                {color: 0xeeeeee, map: new THREE.TextureLoader().load('assets/textures/Iron_Block.png')},
+            0.2, 0.7), 0);
         }else if(levels.boxPos[i][3] == "G"){
-            block.material = new THREE.ShaderMaterial(
-                {
-                    uniforms: __shaderT3.uniforms,
-                    vertexShader: __shaderT3.vertexShader,
-                    fragmentShader: __shaderT3.fragmentShader
-                }
-            );
-        }
-        else if(levels.boxPos[i][3] == "D"){
-            block.material = new THREE.ShaderMaterial(
-                {
-                    uniforms: __shaderT4.uniforms,
-                    vertexShader: __shaderT4.vertexShader,
-                    fragmentShader: __shaderT4.fragmentShader
-                }
-            );
+            block.material = Physijs.createMaterial(new THREE.MeshStandardMaterial(
+                {color: 0xeeeeee, map: new THREE.TextureLoader().load('assets/textures/Gold_Block.png')},
+            0.2, 0.7), 0);
+        }else if(levels.boxPos[i][3] == "D"){
+            block.material = Physijs.createMaterial(new THREE.MeshStandardMaterial(
+                {color: 0xeeeeee, map: new THREE.TextureLoader().load('assets/textures/Diamond_Block.png')},
+            0.2, 0.7), 0);
         }
         objects.push(block);
         scene.add(block);
